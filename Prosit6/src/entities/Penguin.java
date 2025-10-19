1d0211e6f73e3601756ae6ff46e4a54622730821
Mahdi Hamzaoui <mohamedmahdi.hamzaoui@esprit.tn>
2025-10-19 22:39:52 +0100

    Compte Rendu Prosit 6

---
+++ Prosit6/src/entities/Penguin.java
@@ -0,0 +1,26 @@
+package entities;
+
+public class Penguin extends Aquatic{
+    protected float swimmingDepth;
+
+    public Penguin(String family, String name, int age, boolean isMammal, String habitat, float swimmingDepth) {
+        super(family, name, age, isMammal, habitat);
+        this.swimmingDepth = swimmingDepth;
+    }
+
+    public float getSwimmingDepth() {
+        return swimmingDepth;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString().replace("]", "") +
+                ", SwimmingDepth=" + swimmingDepth +
+                "]";
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("This penguin is swimming.");
+    }
+}
