b3fef6ca9b5f28036ae88eb3c03677bda95e4d4a
Mahdi Hamzaoui <mohamedmahdi.hamzaoui@esprit.tn>
2025-11-07 18:43:14 +0100

    Compte Rendu Prosit 7

---
+++ Prosit7/src/entities/Terrestrial.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Terrestrial extends Animal{
+    protected int nbrLegs;
+
+    public Terrestrial(String family, String name, int age, boolean isMammal, int nbrLegs) {
+        super(family, name, age, isMammal);
+        this.nbrLegs = nbrLegs;
+    }
+
+    @Override
+    public String toString() {
+        return "Terrestrial [Family=" + getFamily() +
+                ", Name=" + getName() +
+                ", Age=" + getAge() +
+                ", isMammal=" + getMammal() +
+                ", NbrLegs=" + nbrLegs +
+                "]";
+    }
+}
